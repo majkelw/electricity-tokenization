b01a7db3a9081e656f3ca420d5baf5c1acece63f
Damian <qwerty.dgb@gmail.com>
2023-04-07 21:58:22 +0200

    Added new functions to the core.

--- blockchain_core/Core.py
+++ blockchain_core/Core.py
@@ -18,6 +18,10 @@ class Core():
     users_id = []
     wallets = []
     tokens = []
+    
+    HASH = '0000000000000000000000000000000000000000000000000000000000000000'
+    
+    CoreStats = Enum('CoreStats', ['STATE_OK', 'TOKEN_NOT_EXIST', 'USER_NOT_EXIST', 'WALLET_NOT_EXIST', 'USER_EXIST'])
 
     def contruct_wallets_from_blockchain(self, blockChain):
         walletIdx = 0
@@ -28,9 +32,6 @@ class Core():
 
                 if pool.param == 3:
                     state, wallet = Wallet.construct(pool.id_1)
-                    #################################################
-                    Wallet.add_amount(wallet, 250)
-                    #################################################
                     
                     self.wallets.append(wallet)
                     self.users_id.append(pool.id_1)
@@ -55,15 +56,108 @@ class Core():
                     Wallet.add_operation(self.wallets[index2[0]], operation)
                     Wallet.add_amount(self.wallets[index2[0]], 1)
                     self.tokens.append(pool.id_1)
-        
-        st, st_ = Wallet.print_status(self.wallets[0])
-        print(st_)
-        st, st_ = Wallet.print_transactions(self.wallets[0])
-        print(st_)
-        st, st_ = Wallet.print_operations(self.wallets[0])
-        print(st_)
+                    
+                elif pool.param == 2:
+                    try:
+                        self.tokens.remove(pool.id_1)
+                        index2 = [index for index in range(len(self.users_id)) if self.users_id[index] == pool.id_2]
+                        state, operation = Operation.construct(pool.id_2, 1, 'DEL TOKEN', str(now))
+                        Wallet.add_operation(self.wallets[index2[0]], operation)
+                        Wallet.remove_amount(self.wallets[index2[0]], 1)
+                    except ValueError:
+                        print()
         
         for i in range(0, len(self.wallets)):
             state = Wallet.save_wallet(self.wallets[i], i)
 
+    def print_wallet(self, wallet):
+        print("GENERAL OVERWIEV")
+        state, str_ = Wallet.print_status(wallet)
+        print(str_)
+        print("TRANSACTIONS LIST")
+        state, str_ = Wallet.print_transactions(wallet)
+        print(str_)
+        print("OPERATIONS LIST")
+        state, str_ = Wallet.print_operations(wallet)
+        print(str_)
+
+    def add_transaction(self, user1_id, user2_id, amount, blockChain):
+        now = datetime.datetime.now()
+
+        index1 = [index for index in range(len(self.users_id)) if self.users_id[index] == user1_id]
+        index2 = [index for index in range(len(self.users_id)) if self.users_id[index] == user2_id]
+
+        if index1 and index2:
+            state, wallet1 = Wallet.read_wallet(index1[0])
+            state, wallet2 = Wallet.read_wallet(index2[0])
+
+            state = Wallet.remove_amount(wallet1, amount)
+
+            if state.value == Wallet.WalletStats.STATE_OK.value:
+                state = Wallet.add_amount(wallet2, amount)
+
+                state, transaction1 = Transaction.construct(user1_id, user2_id, amount, 'OUT', str(now))
+                state, transaction2 = Transaction.construct(user1_id, user2_id, amount, 'IN', str(now))
+
+                state = Wallet.add_transaction(wallet1, transaction1)
+                state = Wallet.add_transaction(wallet2, transaction2)
             
+                state = Wallet.save_wallet(wallet1, index1[0])
+                state = Wallet.save_wallet(wallet2, index2[0])
+            
+                self.wallets[index1[0]] = wallet1
+                self.wallets[index2[0]] = wallet2
+                
+                state, new_pool = Pool.construct(user1_id, poolParam.SEND.value, user2_id, amount, str(now), self.HASH)
+                state = blockChain.add_pool(new_pool)
+                
+            else:
+                return (self.CoreStats.WALLET_NOT_EXIST)
+            
+        else:
+            return (self.CoreStats.USER_NOT_EXIST)
+            
+    def add_token(self, token, userID, blockchain):
+        now = datetime.datetime.now()
+
+        state, new_pool = Pool.construct(token, poolParam.nTOKEN.value, userID, 1, str(now), self.HASH)
+        state = blockchain.add_pool(new_pool)
+        
+        index1 = [index for index in range(len(self.users_id)) if self.users_id[index] == userID]
+        
+        if index1:
+            state, wallet1 = Wallet.read_wallet(index1[0])
+            state, operation1 = Operation.construct(userID, 1, 'NEW TOKEN', str(now))
+            state = Wallet.add_operation(wallet1, operation1)
+            state = Wallet.add_amount(wallet1, 1)
+            state = Wallet.save_wallet(wallet1, index1[0])
+            self.wallets[index1[0]] = wallet1
+
+            return (self.CoreStats.STATE_OK)
+        else:
+            return (self.CoreStats.USER_NOT_EXIST)
+            
+    def add_user(self, userID, blockchain):
+        index1 = [index for index in range(len(self.users_id)) if self.users_id[index] == userID]
+        
+        if not index1:
+            now = datetime.datetime.now()
+            state, wallet = Wallet.construct(userID)
+
+            #################################################
+            #Wallet.add_amount(wallet, 0)
+            #################################################
+
+            self.wallets.append(wallet)
+            self.users_id.append(userID)
+                
+            state = Wallet.save_wallet(wallet, len(self.wallets)-1)
+        
+            state, new_pool = Pool.construct(userID, poolParam.nUSER.value, userID, 1, str(now), self.HASH)
+            state = blockchain.add_pool(new_pool)        
+
+            return (self.CoreStats.STATE_OK)
+        else:
+            return (self.CoreStats.USER_EXIST)
+        
+        
--- blockchain_core/main.py
+++ blockchain_core/main.py
@@ -14,60 +14,44 @@ from Wallet.wallet import Wallet
 from Wallet.transaction import Transaction
 from Core import Core
 
+import random
+import hashlib
+
 HASH = '0000000000000000000000000000000000000000000000000000000000000000'
 NONE = '----------------------------------------------------------------'
 user1_id = 'f71f0c070d1ece7c135da3d8843df403c781fa4bf83ea9f3f0995108ee52d532'
 user2_id = '89b98fc3e796e750ebce49c63b69e6fa4e6c3f096b9b93bfba5bcc48ab0c9ad5'
-token = ['df3e6b0bb66ceaadca4f84cbc371fd66e04d20fe51fc414da8d1b84d31d178de',
-         'd8cc7aed3851ac3338fcc15df3b6807b89125837f77a75b9ecb13ed2afe3b49f',
-         '5d6b091416885eaa91283321b69dc526fc42c97783e4cdfdff7a945e3be1f9ef',
-         '4cb0ea499ca7177d32b4deb6e251d0a8f857f91d078af209b7d354528ef62201',
-         'aa1f6f7084d4030e09f5542aa22e7947a81cec2966a251da748cfb501ace6ddc',
-         '8675c9c1f6558212f7f95f5308c03655067e925157ed6e65f024aefadfc7d64f']
+
+def calculate_new_HASH():
+    random_bits = random.getrandbits(128)
+    str_ = str(random_bits)
+    return hashlib.sha256(str_.encode('utf-8')).hexdigest()
 
 def main():
     now = datetime.datetime.now()
            
     blockChain = Blockchain(BlockchainInit.SOURCE)
-    #state, my_item = Pool.construct(user1_id, poolParam.SEND.value, user2_id, 50.0, str(now), HASH)
-    #state, my_item = Pool.construct(token[1], poolParam.nTOKEN.value, user1_id, 1, str(now), HASH)
-    #blockChain.add_pool(my_item)
     
     core = Core()
     core.contruct_wallets_from_blockchain(blockChain)
     
+    #core.add_user(user2_id, blockChain)
     
+    #for i in range(0, 50):
+    #    hash1 = calculate_new_HASH()
+    #    core.add_token(hash1, user1_id, blockChain)
     
-    #state, bb = blockChain.get_block(1)
-    
-    #st, pp = Block.get_pool(1, bb)
-    #st2, str_ = Pool.print_pool(pp)
-    #print(str_)
-    
-    ###############
-    #state, wallet = Wallet.construct(user1_id)
-    #state = Wallet.save_wallet(wallet, 1)
-    #state, pool = Pool.construct(user2_id, poolParam.nUSER.value, NONE, 1, str(now), HASH)
-    #state = blockChain.add_pool(pool)
-    ###############
-    
-    #transaction = Transaction.construct(user1_id, user2_id, 50.0, 'IN', str(now))
-    #Wallet.add_transaction(wallet, transaction)
+    #core.add_transaction(user1_id, user2_id, 50, blockChain)
+    #core.add_transaction(user1_id, user2_id, 1, blockChain)
+    #core.add_transaction(user1_id, user2_id, 19, blockChain)
+    #core.add_transaction(user1_id, user2_id, 25, blockChain)
+
 
     st = blockChain.print_blocks() # blockChain.print_blocks([2])
     print (st)
     
-    #state, wallet = Wallet.read_wallet(2)
-    #print(state)
-    
-    #st, wallet = Wallet.add_amount(wallet, 100.0)
-    #print(wallet.bilance)
-    #st, wallet = Wallet.remove_amount(wallet, 99.0)
-    #print(wallet.bilance)
-    
-    #sdsd, wallet = Wallet.add_transaction(wallet, user1_id, user2_id, 50.0)
-    #print(wallet.transactions)
-    #print(wallet.bilance)
+    core.print_wallet(core.wallets[0])
+    core.print_wallet(core.wallets[1])
     
 if __name__ == "__main__":
     main()
