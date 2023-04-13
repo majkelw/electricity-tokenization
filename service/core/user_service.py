4bb3d52af1fe2b505ce82e1e3fade2603e96e95f
majkelw <michalwik@vp.pl>
2023-04-14 00:24:33 +0200

    seed (words generation) pphase 1

--- service/core/user_service.py
+++ service/core/user_service.py
@@ -1,5 +1,9 @@
+from mnemonic import Mnemonic
+
+
 class UserService:
     users_ids = ["001", "002", "003", "004"]
+    mnemonic = Mnemonic("english")
     """
     mock na potrzeby symulatora, w tej klasie mozna zaimplementowac lub wywolac funkcje,
     ktora sprawdzi czy dany uzytkownik istnieje
@@ -7,3 +11,7 @@ class UserService:
 
     def exist_by_id(self, user_id):
         return True if user_id in self.users_ids else False
+
+    def create_user(self):
+        return {"detail": "New user added, please save generated words in safe place",
+                "words": self.mnemonic.generate(strength=256).split(" ")}
--- service/main.py
+++ service/main.py
@@ -3,9 +3,11 @@ import uvicorn
 
 from model.energy_body import EnergyBody
 from core.token_service import TokenService
+from service.core.user_service import UserService
 
 app = fastapi.FastAPI()
 token_service = TokenService()
+user_service = UserService()
 
 
 @app.post("/create-token")
@@ -14,6 +16,11 @@ async def create_token(energy_body: EnergyBody):
     if response_code != 200:
         raise fastapi.HTTPException(response_code, message)
     return {"detail": message}
+
+
+@app.post("/create-user")
+async def create_user():
+    return user_service.create_user()
 
 
 if __name__ == "__main__":
