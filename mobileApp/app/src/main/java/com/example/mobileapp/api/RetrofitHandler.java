bb470cffc900c9dc59acb35f0f89ed16fd3a3ff2
majkelw <michalwik@vp.pl>
2023-05-18 00:23:44 +0200

    MobileApp: RetrofitHanlder refactoring

--- mobileApp/app/src/main/java/com/example/mobileapp/api/RetrofitHandler.java
+++ mobileApp/app/src/main/java/com/example/mobileapp/api/RetrofitHandler.java
@@ -1,20 +1,18 @@
 package com.example.mobileapp.api;
 
-import android.widget.Toast;
-
-import com.example.mobileapp.activity.SignUpActivity;
-
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitHandler {
 
     private static Retrofit retrofit;
+    private final static String URL = "http://192.168.156.93";
+    private final static String PORT = "8080";
 
     public static Retrofit getInstance() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
-                    .baseUrl("http://192.168.156.93:8080")
+                    .baseUrl(URL + ":" + PORT)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
